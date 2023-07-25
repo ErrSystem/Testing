7c60007bf15faeb63b776599d94a9f1bba9ca77f
ErrSys <62564023+gameur-5500@users.noreply.github.com>
2023-07-25 17:23:18 +0100

    Edited the code!

--- js/script.js
+++ js/script.js
@@ -2,23 +2,21 @@ function model(color, text, ID) {
     this.color = color;
     this.text = text;
     this.id = ID;
-    this.GetThis = function (){
-      return this;
-    }
+    Objs.push(this)
 }
 function EditDom(Element) {
-    const p = document.getElementById(Element.id);
-    p.style.color = Element.color;
-    p.innerHTML = Element.text;
-    console.log(Element);
+    for (Obj of Objs) {
+        const p = document.getElementById(Obj.id);
+        p.style.color = Obj.color;
+        p.innerHTML = Obj.text;
+        console.log(Obj);
+    }
 }
+
+const Objs = [];
 
 const paragraph = new model('red', 'yeah bro', 'contener');
 const secondParagraph = new model('yellow', 'no way man', 'not_contener');
 const thirdParagraph = new model('blue', 'woah', 'Obj');
 
-const text = [paragraph, secondParagraph, thirdParagraph];
-
-for (Element of text) {
-    EditDom(Element.GetThis());
-}
+EditDom();
