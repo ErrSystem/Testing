777aaa1ced1efeda1680328b99e3bec7535526ca
ErrSys <teamgamingv2@gmail.com>
2023-07-28 02:53:36 +0100

    Fixed JS and JSON files
    - Fixed some bugs!

--- js/Test2.js
+++ js/Test2.js
@@ -1,23 +1,19 @@
-let input = '';
-let Output = '';
-
 document.addEventListener('keypress', event => {
-    if (event.key === 'Enter') {
-        input = document.getElementById('input').value; // Gets the input's value
-        // the 3 next lines are used to get info from JSON file 
-        fetch('log.json')
-        .then(response => response.json())
-        .then(json => {
-            json.users.forEach(user => {  // for each user do:
-                if (input == user.email) { // if the input's value == email from any existing account do:
-                    Output = "The name of this user is "+user.name+" his ID is "+user.id+" and he signed to this website the "+user.accountDateCreation; // text that will be written
-                }
-                else {
-                    console.log('nope');
+    if (event.key === 'Enter') { // if enter is pressed
+        let Output = ''; // Output's value
+        let input = document.getElementById('input').value; // input's value
+
+        fetch('log.json') // get Json file
+        .then(response => response.json()) // get response
+        .then(JSON_File => { 
+            JSON_File.users.forEach(user => { // for each user in the JSON file 
+                if (input == user.email) { // if the input's value = an account's email 
+                    Output = "The name of this user is "+user.name+" his ID is "+user.id+" and he signed to this website the "+user.accountDateCreation; // what will be writed in the output
+                    console.log(Output); // prints Output's value (just for testing will be deleted later) 
+                    document.getElementById('Output').innerHTML = Output; // adds Output's variable value to the output located in HTML
+                    input = ''; // clears the input
                 }
             });
-        });
-        document.getElementById('input').value = '';
-        document.getElementById('Output').innerHTML = Output;
+        })
     }
 })
