5cc21a6c8e77e6eba4be515eeef6de2cc9fdc530
ErrSys <teamgamingv2@gmail.com>
2023-07-30 04:16:42 +0100

    more advanced JSON fetch

--- js/Test1.js
+++ js/Test1.js
@@ -1,15 +1,15 @@
-fetch('users.json')
-.then(response => {
-    if (response.ok) {
-        console.log('Success');
-    }
-    else {
-        console.log('Error')
-    }
-})
-.then(data => console.log(data))
-.catch(error => console.log(error))
+const JSON_file = 'users.json';
 
-let promise = () => {
-
+let fetch_Json = () => {
+    fetch(JSON_file)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Network response isnt ok: ${response.status}`)
+        }
+        return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => console.log(`Error fetching data: ${error}`))
 }
+
+window.onload = () => fetch_Json();
