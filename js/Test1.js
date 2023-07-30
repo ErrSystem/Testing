7b4a406f1a148c02c4b81e4d0ec3f132d61967fb
ErrSys <teamgamingv2@gmail.com>
2023-07-30 15:47:52 +0100

    Updated .Json file and Js fetch system

--- js/Test1.js
+++ js/Test1.js
@@ -1,15 +1,30 @@
-const JSON_file = 'users.json';
-
-let fetch_Json = () => {
-    fetch(JSON_file)
+const fetch_JSON = source => {
+    return fetch(source)
     .then(response => {
-        if (!response.ok) {
-            throw new Error(`Network response isnt ok: ${response.status}`)
+        if (!response.ok){
+            throw new Error(`Error while getting response: ${response.status}`)
         }
-        return response.json();
+        return response.json()
     })
-    .then(data => console.log(data))
-    .catch(error => console.log(`Error fetching data: ${error}`))
+    .then(Json_data => Json_data)
+    .catch(error => {
+        console.log(new Error(`Error while fetching data: ${error}`));
+        return null;
+    });
 }
 
-window.onload = () => fetch_Json();
+document.addEventListener('keypress', event => {
+    if (event.key === 'Enter') {
+        const Json_File = 'users.json';
+        const input = document.getElementById('input').value;
+        const data = fetch_JSON(Json_File);
+        for (user in data){
+            if (user.email === input){
+                console.log('Nice')
+            }
+            else{
+                console.log('Nope');
+            }
+        }
+    }
+})
