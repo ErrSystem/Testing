827e83df9ae05c8549d72c6c949d872c0a57ea97
gameur-5500 <62564023+gameur-5500@users.noreply.github.com>
2023-08-03 17:43:02 +0200

    Testing APIs
    - Warning immense lag when loading page!

--- js/Test3.js
+++ js/Test3.js
@@ -1,19 +1,26 @@
-const APIurl = 'https://api-football-v1.p.rapidapi.com/v3/timezone';
-const options = {
-    method: 'GET',
-    headers:{
-        'X-RapidAPI-Key': 'e2867e76cfmsh8228189f18bddd1p1b0996jsn0f696056ff89',
-		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-    }
-}
+let apiData = {};
 async function FetchData() {
+    const APIurl = 'https://api.publicapis.org/entries';
+    const options = {
+    method: 'GET',
+}
     try {
         const response = await fetch(APIurl, options)
-        const result = await response.text()
-        console.log(result);
-     }
-     catch (error){
-         console.error(error)
-     }
+        const data = await response.json()
+        console.log(data)
+        ChangeHtml(data)
+    }
+    catch (error){
+        console.error(error);
+    }
 }
+
+const ChangeHtml = (data) => {
+    const list = document.getElementById('list');
+    for (let i=0; i < data.count; i++){
+        const api = data.entries[i]
+        list.innerHTML += `<li>${api.API}: ${api.Description}</li><br><br>`;
+    }
+}
+
 FetchData();
