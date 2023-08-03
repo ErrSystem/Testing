ead9b913368b582e3d73459a986d5500a3cbff5b
gameur-5500 <62564023+gameur-5500@users.noreply.github.com>
2023-08-03 18:34:07 +0200

    Making more tests with APIs

--- js/Test1.js
+++ js/Test1.js
@@ -1,30 +1,26 @@
-const fetch_JSON = source => {
-    return fetch(source)
-    .then(response => {
-        if (!response.ok){
-            throw new Error(`Error while getting response: ${response.status}`)
-        }
-        return response.json()
-    })
-    .then(Json_data => Json_data)
-    .catch(error => {
-        console.log(new Error(`Error while fetching data: ${error}`));
-        return null;
-    });
+let data;
+async function FetchData() {
+    const APIurl = 'https://api.publicapis.org/entries';
+    const options = {
+    method: 'GET',
+}
+    try {
+        const response = await fetch(APIurl, options)
+        data = await response.json()
+        // key detection
+        window.addEventListener('keypress', event => {
+            if (event.key === 'Enter'){
+                addToHTML(); // returns to function
+            }
+        })
+    }
+    catch (error){
+        console.error(error);
+    }
 }
 
-document.addEventListener('keypress', event => {
-    if (event.key === 'Enter') {
-        const Json_File = 'users.json';
-        const input = document.getElementById('input').value;
-        const data = fetch_JSON(Json_File);
-        for (user in data){
-            if (user.email === input){
-                console.log('Nice')
-            }
-            else{
-                console.log('Nope');
-            }
-        }
-    }
-})
+function addToHTML() {
+  console.log(data)
+}
+
+FetchData()
--- js/Test3.js
+++ js/Test3.js
@@ -1,4 +1,4 @@
-let apiData = {};
+let data;
 async function FetchData() {
     const APIurl = 'https://api.publicapis.org/entries';
     const options = {
@@ -6,21 +6,50 @@ async function FetchData() {
 }
     try {
         const response = await fetch(APIurl, options)
-        const data = await response.json()
-        console.log(data)
-        ChangeHtml(data)
+        data = await response.json()
+        // key detection
+        window.addEventListener('keypress', event => {
+            if (event.key === 'Enter'){
+                addToHTML(); // returns to function
+            }
+        })
     }
     catch (error){
         console.error(error);
     }
 }
 
-const ChangeHtml = (data) => {
+const addToHTML = () => {
     const list = document.getElementById('list');
-    for (let i=0; i < data.count; i++){
-        const api = data.entries[i]
-        list.innerHTML += `<li>${api.API}: ${api.Description}</li><br><br>`;
+    const results = search();
+    for (result of results){
+        if (!isEven(i)){
+            list =+ `<li>${result}: ${result}</li> <br><br>`;
+        }
     }
+}
+
+const search = () => {
+    let input = document.getElementById('input');
+    let results = [];
+    for (let i=0; i < data.count; i++){
+        const api = data.entries[i];
+        if (input.length <= api.API.length && input == api.API.slice(0, input.length)) {
+            results += api.API + api.Description;
+            console.log(results);
+        }
+        else if (i == data.count) {
+            console.log(results);
+            return results;
+        }
+    }
+}
+
+const isEven = number => {
+    if (number % 2 == 0) {
+        return true;
+    }
+    return false;
 }
 
 FetchData();
