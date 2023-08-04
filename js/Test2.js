2b0289db20133f4f4f13adadd333b3b2fae75870
gameur-5500 <62564023+gameur-5500@users.noreply.github.com>
2023-08-04 17:06:47 +0200

    Tested some typeof methods

--- js/Test2.js
+++ js/Test2.js
@@ -1,4 +1,10 @@
 let array = ['fruits:', 'apple', 'banana'];
 let array2 = ['vegetals:', 'potato', 'onion'];
 let finalArray = array.concat('|', array2);
-console.log(finalArray.join(' '));
+let verifyString = value => {
+    if (typeof value == 'string'){
+        return 'Wow thats a string!'
+    }
+    return `No bro thats a ${typeof value} not a string`;
+}
+console.log(verifyString(finalArray));
