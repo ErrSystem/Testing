c123223d287064f7c510b3ab895a3070c369cddb
gameur-5500 <62564023+gameur-5500@users.noreply.github.com>
2023-08-04 13:38:38 +0200

    made some tests with buily-in functions

--- js/Test1.js
+++ js/Test1.js
@@ -1,18 +1,19 @@
-let addZero = numbers => {
-    for (let i = 0; numbers.length >= i; i++){
-        if(numbers[i] < 10 && i <= 3){
-            numbers[i] = '0'+numbers[i];
-        }
-        else if(i == numbers.length){
-            return numbers;
+let addZero = (...nums) => {
+    for (let num of nums){
+        if(num < 10){
+            nums[nums.indexOf(num)] = '0'+num;
         }
     }
+    if (nums[nums.length - 1] > 60){
+        return nums.join('/');
+    }
+    return nums.join(':');
 }
 const Getdate = () => {
     let date = new Date();
-    let day = addZero([date.getFullYear(), date.getMonth()+1, date.getDate()]);
-    let time = addZero([date.getHours(), date.getMinutes(), date.getSeconds()]);
-    document.getElementById('p').innerHTML = `Current Date: ${day.join('/')} ${time.join(':')}`;
+    let day = [date.getDate(), date.getMonth()+1, date.getFullYear()];
+    let time = [date.getHours(), date.getMinutes(), date.getSeconds()];
+    document.getElementById('p').innerHTML = `Current Date: ${addZero(...day)} ${addZero(...time)}`;
 }
 document.getElementById('p').innerHTML =  'Loading...';
 setInterval(Getdate, 1000);
