c7648da0aaa0fdc5aa8624208a53ac981de4b39a
gameur-5500 <62564023+gameur-5500@users.noreply.github.com>
2023-08-06 04:11:07 +0200

    Making ToDo

--- js/Test4.js
+++ js/Test4.js
@@ -1,4 +1,7 @@
 let email = 'errsys@gmail.com';
 let array = Array.from(email);
 console.log(array.some(value => value == '@') && array.some(value => value == '.'));
-// or console.log(array.includes('@') && array.includes('.')); thats more simple but wanted to test .some()
+/* Or we can use:
+    let email = 'errsys@gmail.com';
+    console.log(email.includes('@') && email.includes('.')); 
+thats more simple but wanted to test .some() */
