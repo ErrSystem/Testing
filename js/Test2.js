9820f0da34f59738c9c79d28754f85c328acc2d7
ErrSys <teamgamingv2@gmail.com>
2023-09-07 18:09:14 +0100

    restarted learning SASS!

--- js/Test2.js
+++
@@ -1,9 +0,0 @@
-let originalNumber = prompt('number?');
-let powerPrompt = prompt('power?');
-
-let root = () => Math.sqrt(originalNumber).toFixed(2)
-let power = () => Math.pow(originalNumber, powerPrompt).toFixed(2)
-let random = () => (Math.random() * 1000).toFixed(2);
-
-document.querySelector('p').innerHTML += `The square root of this number (${originalNumber}) is ${root()} and its power by ${powerPrompt} is ${power()}`;
-document.querySelector('p').innerHTML += `<br> Here is a random number: ${random()}`;
