9fc54e0f95a72138d258a6d06431f84125bcba90
ErrSys <teamgamingv2@gmail.com>
2023-09-13 19:25:19 +0100

    Finished more than 50% of the "main" work!

--- js/sectionSlider.js
+++ js/sectionSlider.js
@@ -1,19 +1,15 @@
+// variables
 let scrollIndex = 0;
 let cooldown = true;
 let touchStart;
 const sliderContener = document.querySelector('.slider');
 
-document.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        elementsFadeIn(document.querySelector('.firstSlide'));
-    }, 1000);
-})
-
+// detectors
 const wheelDetector = event => {
     let direction = event.deltaY;
     if (cooldown) {
         cooldown = false;
-        if (direction == 100 && scrollIndex != 3) {
+        if (direction == 100 && scrollIndex != 6) {
             scrollDown();
         } else if(direction == -100 && scrollIndex != 0){
             scrollUp();
@@ -50,14 +46,15 @@ const touchDetector = event => {
     }, 500);
 }
 
+// functions
 const scrollUp = () => {
     if (scrollIndex != 0) {
         const newSlide = sliderContener.children[scrollIndex-1];
         const currentSlide = sliderContener.children[scrollIndex];
-        sliderContener.style.animation = 'fadeOut 0.5s';
+        sliderContener.style.opacity = '0';
         setTimeout(() => {
-            newSlide.className = '';
-            currentSlide.className = 'hidden';
+            newSlide.className = `${newSlide.classList[0]}`;
+            currentSlide.className = `${currentSlide.classList[0]} hidden`;
             sliderContener.style.opacity = 0; 
             sliderContener.style.animation = 'fadeIn 0.5s';
             setTimeout(() => {
@@ -67,19 +64,19 @@ const scrollUp = () => {
         }, 250);
         scrollIndex--;
         document.querySelector('body').style.overflow = 'hidden';
-        document.querySelector('.scrollDown').style.opacity = '1';
+        document.querySelector('.mouseDown').style.opacity = '1';
     }
 }
 
 const scrollDown = () => {
-    if (scrollIndex < sliderContener.children.length -2) {
+    if (scrollIndex < sliderContener.children.length - 2) {
         const newSlide = sliderContener.children[scrollIndex+1];
         const currentSlide = sliderContener.children[scrollIndex];
-        sliderContener.style.animation = 'fadeOut 0.5s';
+        sliderContener.style.opacity = '0';
         setTimeout(() => {
             sliderContener.style.opacity = 0; 
-            newSlide.className = '';
-            currentSlide.className = 'hidden';
+            newSlide.className = `${newSlide.classList[0]}`;
+            currentSlide.className = `${currentSlide.classList[0]} hidden`;
             sliderContener.style.opacity = 1; 
             setTimeout(() => {
                 elementsFadeIn(newSlide);
@@ -87,34 +84,47 @@ const scrollDown = () => {
             }, 500);
         }, 250);
         scrollIndex++;
-        if (scrollIndex == 3){
-            document.querySelector('footer').style.display = 'block';
-            document.querySelector('body').style.overflow = 'scroll';
-            document.querySelector('.scrollDown').style.opacity = '0';
-        }
+        // if (scrollIndex == 3){
+        //     document.querySelector('footer').style.display = 'block';
+        //     document.querySelector('body').style.overflow = 'scroll';
+        //     document.querySelector('.scrollDown').style.opacity = '0';
+        // }
     }
 }
 
+
+// animations
 const elementsFadeIn = section => {
     sliderContener.style.opacity = 1;
+    section.style.opacity = 1;
     Array.from(section.children).forEach(element => {
         element.style.display = 'block';
         element.style.opacity = 1;
-        section.children[0].style.paddingTop = '15vh';
-        section.children[0].style.marginBottom = '60px';
     });
+    section.children[1].children[0].style.paddingTop = '15vh';
+    section.children[1].children[0].style.marginBottom = '60px';
 }
 
 const elementsFadeOut = section => {
+    section.style = '';
     Array.from(section.children).forEach(element => {
         element.setAttribute('style', '');
     })
+    Array.from(section.children[1].children).forEach(element => {
+        element.setAttribute('style', '');
+    })
 }
-
+// event listeners
 window.addEventListener('wheel', wheelDetector);
 window.addEventListener('touchstart', e => {
     touchStart = e.touches[0].clientY;
 });
-
 window.addEventListener('touchend', touchDetector);
-// document.querySelector('.scrollDown').addEventListener('click', scrollDown);
+document.querySelector('.mouseDown').addEventListener('click', scrollDown);
+
+// animation when website is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        elementsFadeIn(document.querySelector('.welcomeSlide'));
+    }, 1000);
+})
