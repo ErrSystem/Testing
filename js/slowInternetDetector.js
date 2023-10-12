5944b18e3235305fd1e5f0043a8ecacf39773fc2
ErrSys <teamgamingv2@gmail.com>
2023-10-12 23:10:21 +0100

    Bug Fixes!
    + Fixed slow network script repeat!
    + Made terrace video smaller!
    + Fixed visual bug with slow network notif!

--- js/slowInternetDetector.js
+++ js/slowInternetDetector.js
@@ -2,16 +2,20 @@ let time = 0;
 let stillCounting = true;
 
 setInterval(() => {
-    if (stillCounting === true){
+    if (stillCounting){
         time++;
     }
 }, 100);
 
 document.querySelector('.welcomeSlide video').addEventListener('loadeddata', () => {
-    console.log(time)
-    stillCounting = false;
-    if (time >= 10) {
-        document.querySelector('.warnWeakInternet').style.display = 'block';
-        document.querySelector('.warnWeakInternet').style.opacity = '1';
+    if (stillCounting) {
+        stillCounting = false;
+        if (time >= 10) {
+            document.querySelector('.warnWeakInternet').style.display = 'block';
+            document.querySelector('.warnWeakInternet').style.opacity = '1';
+        } else {
+            document.querySelector('.warnWeakInternet').style.display = 'none';
+            document.querySelector('.warnWeakInternet').style.opacity = '0';
+        }
     }
 })
