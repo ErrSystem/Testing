668eb2f0caa20c2cd4b1a234476f2a6b716c37b9
ErrSys <teamgamingv2@gmail.com>
2023-10-14 20:52:18 +0100

    Didnt upload newer files! (fix)

--- test/test.js
+++ test/test.js
@@ -1,14 +1,17 @@
-const consoleStyles = "Color: red; font-weight: 900; padding: 20px; text-shadow: 1px 1px 20px white";
+const consoleStyles = "Color: red; font-weight: 900; padding: 20px; text-shadow: 1px 1px 20px white; background: black";
 const arr = [
     "Pizza",
     "Lasagne",
     "McDo"
 ]
+const age = 19;
 
 const logWithStyle = (str) => {
     console.log(`%c${str}`, consoleStyles);
 }
 
+const agestr = age >= 18 ? "Old nigga" : "Young nigga";
 
-logWithStyle('hey');
-console.table(arr)
+
+logWithStyle(agestr);
+console.table(arr);
