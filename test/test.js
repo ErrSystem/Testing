5f865c51a9b0bec81c4776e861dbae331bc6aa9f
ErrSys <teamgamingv2@gmail.com>
2023-10-14 20:27:12 +0100

    Just doing some tests

---
+++ test/test.js
@@ -0,0 +1,10 @@
+const consoleStyles = "Color: red; font-weight: 900; padding: 20px; text-shadow: 1px 1px 20px white";
+console.log("%cHey", consoleStyles)
+
+const arr = [
+    "Pizza",
+    "Lasagne",
+    "McDo"
+]
+
+console.table(arr)
