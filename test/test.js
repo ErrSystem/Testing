d0611bbff8b0629a2a75af75d327f0c9e6e8d6af
ErrSys <teamgamingv2@gmail.com>
2023-10-14 20:51:30 +0100

    Other tests!

--- test/test.js
+++ test/test.js
@@ -1,10 +1,14 @@
 const consoleStyles = "Color: red; font-weight: 900; padding: 20px; text-shadow: 1px 1px 20px white";
-console.log("%cHey", consoleStyles)
-
 const arr = [
     "Pizza",
     "Lasagne",
     "McDo"
 ]
 
+const logWithStyle = (str) => {
+    console.log(`%c${str}`, consoleStyles);
+}
+
+
+logWithStyle('hey');
 console.table(arr)
