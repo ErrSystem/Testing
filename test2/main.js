a8ed0826463d9618f95ae05d480e6353b73e399c
ErrSys <teamgamingv2@gmail.com>
2023-11-28 23:23:20 +0100

    update

--- test2/main.js
+++ test2/main.js
@@ -4,7 +4,11 @@ let num = parseInt(random * 10);
 const Process = () => {
     if (parseInt(document.querySelector('input').value) === num) {
         document.querySelector('p').innerText = "Congrats!";
+        random = Math.random();
+        num = parseInt(random * 10);
     } else {
         document.querySelector('p').innerText = "You are a nigga, it was "+ num;
+        random = Math.random();
+        num = parseInt(random * 10);
     }
 }
